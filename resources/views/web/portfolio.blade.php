d04e45d2fcf44069d3f368a7b77cb8ca84f65712
captainwasi123 <captain.wasi@gmail.com>
2022-06-24 19:41:22 +0500

    Portfolio

--- resources/views/web/portfolio.blade.php
+++ resources/views/web/portfolio.blade.php
@@ -57,7 +57,7 @@
             @foreach($portfolio as $val)
                <div class="col-md-4 col-lg-4 col-sm-6 col-12 custom-pad1">
                   <div class="card-image">
-                     <a href="{{URL::to('/public/storage/portfolio/large/'.$val->large_image)}}" data-fancybox="gallery" data-caption="{{$val->title}}">
+                     <a href="{{URL::to('/public/storage/portfolio/')}}/{{empty($val->large_image) ? $val->image : 'large/'.$val->large_image}}" data-fancybox="gallery" data-caption="{{$val->title}}">
                      <img src="{{URL::to('/public/storage/portfolio/'.$val->image)}}" alt="Image Gallery">
                      </a>
                   </div>
